39377e4a113c0708c01c22832fb7bbc69026f631
DaseiNaN <dasein_csd@qq.com>
2022-05-01 16:31:45 +0800

    add feat extraction scripts

--- scripts/schedule.sh
+++ scripts/schedule.sh
@@ -2,6 +2,17 @@
 # Shedule execution of many runs
 # Run from root folder with: bash scripts/schedule.sh
 
-python train.py trainer.max_epochs=5
+# python train.py trainer.max_epochs=5
+# python train.py trainer.max_epochs=10 logger=csv
 
-python train.py trainer.max_epochs=10 logger=csv
+stage=0
+
+data_dir=/home/dasein/Projects/MMD/data/EATD-Corpus
+
+if [ $stage -eq 0 ]; then
+    # Extract audio feats
+    python src/utils/feats/audio_feats.py -i $data_dir
+    # Extract text feats
+    python src/utils/feats/text_feats.py -i $data_dir
+fi
+
---
+++ src/utils/metrics.py
@@ -0,0 +1,24 @@
+import numpy as np
+from sklearn.metrics import confusion_matrix
+
+
+def standard_confusion_matrix(y_true, y_pred):
+    """ Standard confusion matrix
+    
+    ====================================
+    |                |      Truth      |
+    |----------------+-----------------|
+    |                |   True   False  |
+    | Pred  Positive |    TP     FP    |
+    |       Negative |    FN     TN    |
+    ====================================
+    """
+    
+    [[tn, fp], [fn, tp]] = confusion_matrix(y_true, y_pred)
+    return np.array([[tp, fp], [fn, tn]])
+
+
+def measure_performance(y_true, y_pred):
+    y_pred = y_pred.max(dim=1, keepdim=True)[1]
+    conf_matrix = standard_confusion_matrix(y_true, y_pred.numpy())
+    return y_pred, conf_matrix
